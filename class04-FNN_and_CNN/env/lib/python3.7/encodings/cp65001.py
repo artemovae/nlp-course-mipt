aba17bfdfd46a4ec856038ef88a4531247009e7c
katya.artemova <ek.chernyak@gmail.com>
2019-02-27 13:54:16 +0300

    add class04

---
+++ class04-FNN_and_CNN/env/lib/python3.7/encodings/cp65001.py
@@ -0,0 +1,1 @@
+/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/encodings/cp65001.py
---
+++ class04-FNN_and_CNN/env/share/jupyter/lab/static/vega.0adc8a452b66056a4f6e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[2],{10:function(n,o){},11:function(n,o){}}]);
+//# sourceMappingURL=vega.0adc8a452b66056a4f6e.js.map
